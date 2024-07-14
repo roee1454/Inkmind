cfd2179a066b137b207c203a96e8a646ea0ae8f5
roee heily <roee1454@gmail.com>
2024-07-14 22:51:08 +0300

    fix

--- app/api/refreshToken/route.ts
+++ app/api/refreshToken/route.ts
@@ -28,7 +28,7 @@ export async function GET(req: NextRequest) {
         return NextResponse.json({ message: 'Token refreshed and updated successfully', token: data.access_token });
       } else {
         const vercelError = await vercelResponse.json();
-        return NextResponse.json({ error: vercelError.message }, { status: 500 });
+        return NextResponse.json({ error: vercelError.message, message: "error" }, { status: 500 });
       }
     } else {
       return NextResponse.json({ error: 'Failed to refresh token' }, { status: 500 });
