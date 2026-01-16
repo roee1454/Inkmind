9cc501da341bfbf50e290973934bf68ce43a2183
roee1454 <roee1454@gmail.com>
2026-01-16 19:18:04 +0200

    Another deplyoment

--- lib/enable.ts
+++ lib/enable.ts
@@ -1,4 +1,4 @@
 export const ENABLE_CDN_URL =
-  process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
-    ? "https://cdn.enable.co.il/licenses/enable-L23490xm54kyboqr-1223-77789/init.js"
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "production" || process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
+    ? "https://cdn.enable.co.il/licenses/enable-L23490xm54kyboqr-1223-61173/init.js"
     : "https://cdn.enable.co.il/licenses/enable-L23490xm54kyboqr-1223-77791/init.js";
