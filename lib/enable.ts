e0bca70d2cd1095355ca7d274632a813e34ca7f3
roee1454 <roee1454@gmail.com>
2026-01-16 18:29:58 +0200

    Updated enable url

--- lib/enable.ts
+++ lib/enable.ts
@@ -1,4 +1,4 @@
 export const ENABLE_CDN_URL =
   process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
-    ? "https://cdn.enable.co.il/licenses/enable-L23490xm54kyboqr-1223-77789/init.js"
+    ? "https://cdn.enable.co.il/licenses/enable-L23490xm54kyboqr-1223-77796/init.js"
     : "https://cdn.enable.co.il/licenses/enable-L23490xm54kyboqr-1223-77791/init.js";
